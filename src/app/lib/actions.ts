9c4f244612a2720e080c8b4206ec82d17d2f8097
Thomas Van Tuycom <thomas.vantuycom33@gmail.com>
2025-10-17 16:47:53 +0200

    Added contact form email

---
+++ src/app/lib/actions.ts
@@ -0,0 +1,21 @@
+'use server';
+
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function sendEmail(formData: FormData) {
+    const email = formData.get('email') as string;
+    const message = formData.get('message') as string;
+
+    await resend.emails.send({
+        from: 'Portfolio Contact <onboarding@resend.dev>',
+        to: 'thomas.vantuycom33@gmail.com',
+        subject: 'New portfolio contact response registered',
+        html: `
+      <h2>New response to contact form of protfolio</h2>
+      <p>Email: ${email}</p>
+      <p>Message:<br>${message}</p>
+    `,
+    });
+}
--- src/app/ui/contact/contact.tsx
+++ src/app/ui/contact/contact.tsx
@@ -1,12 +1,16 @@
 import { bakehaus, karla } from "../fonts";
 import Glassdiv from "../glassdiv";
 import Image from "next/image";
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+import { sendEmail } from "../../lib/actions"
 
 export default function Contact({
     id,
-}:{
+}: {
     id: string,
 }) {
+
     return (
         <div id={id} className="flex flex-row justify-center items-center h-[100vh]">
             <div className="flex flex-col w-[40%]">
@@ -21,11 +25,12 @@ export default function Contact({
                     </div>
                     <h2 className={`${bakehaus.className} text-4xl font-regular`}>Get in Touch</h2>
                 </div>
-                <form action="" className="flex flex-col space-y-4">
+                <form action={sendEmail} className="flex flex-col space-y-4">
                     <input type="email" name="" id="" />
                     <div className="relative backdrop-blur-xs bg-[#1E1E1E]/20 border border-white/10 shadow-lg overflow-hidden p-2 rounded-4xl w-1/2">
                         <input
                             type="email"
+                            name="email"
                             placeholder="Your email"
                             className={`${karla.className} bg-(--text-primary) w-full px-8 py-4 rounded-3xl text-black font-bold`}
                         />
@@ -33,23 +38,24 @@ export default function Contact({
                     <div className="relative backdrop-blur-xs bg-[#1E1E1E]/20 border border-white/10 shadow-lg overflow-hidden p-2 rounded-4xl w-full h-[30vh]">
                         <textarea
                             placeholder="Your message..."
+                            name="message"
                             className={`${karla.className} bg-(--text-primary) w-full h-full py-4 px-8 rounded-3xl text-black resize-none font-bold`}
                         />
                     </div>
                     <div className=" flex justify-end">
                         <div className="relative backdrop-blur-xs bg-[#1E1E1E]/20 border border-white/10 shadow-lg overflow-hidden p-2 rounded-3xl w-1/4">
-                        <button className="flex flex-row justify-center items-center bg-white w-full rounded-2xl py-4 px-8 gap-2">
-                            <h2 className={`${karla.className} text-[#292C33] text-[16px] font-bold tracking-[.3em]`}>Send</h2>
-                            <div className="relative w-5 h-5">
-                                <Image
-                                    src="/paperplane.png"
-                                    alt="icon"
-                                    fill
-                                    className="object-contain"
-                                />
-                            </div>
-                        </button>
-                    </div>
+                            <button className="flex flex-row justify-center items-center bg-white w-full rounded-2xl py-4 px-8 gap-2">
+                                <h2 className={`${karla.className} text-[#292C33] text-[16px] font-bold tracking-[.3em]`}>Send</h2>
+                                <div className="relative w-5 h-5">
+                                    <Image
+                                        src="/paperplane.png"
+                                        alt="icon"
+                                        fill
+                                        className="object-contain"
+                                    />
+                                </div>
+                            </button>
+                        </div>
                     </div>
                 </form>
             </div>
